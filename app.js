96676b6ccbeaf9a12d6879c52277f445b5277c7f
Shrikant-95 <124565676+Shrikant-95@users.noreply.github.com>
2023-02-21 12:03:03 +0530

    docs: added new js file

---
+++ app.js
@@ -0,0 +1,13 @@
+// console.log("Hey you are importing this from another file")
+
+
+// var userName = prompt("Give your Username")
+// alert("This Script works "+userName)
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+// console.log(btnTranslate);
+
+btnTranslate.addEventListener("click",function clickEventHandler(){
+    console.log("clicked")
+})
